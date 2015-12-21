3e666b792be5853f9dd9aa1de9ddd822e9f16b09
W. Nicholas Greene <wng@csail.mit.edu>
2015-12-21 18:24:53 -0500

    Update quicktile.

--- packages/quicktile.sh
+++ packages/quicktile.sh
@@ -6,14 +6,17 @@ CW=${PWD}
 # Install some dependencies.
 sudo apt-get install -y python python-gtk2 python-xlib python-dbus python-wnck
 
+cp ../config/quicktile.cfg ${HOME}/.config/
+
 mkdir -p ${HOME}/.local/opt
 cd ${HOME}/.local/opt
+
+mkdir -p ${HOME}/.local/lib/python2.7/site-packages
+mkdir -p ${HOME}/.local/lib/python2.7/dist-packages
 
 git clone git://github.com/ssokolow/quicktile
 cd quicktile
 
-./setup.py install --prefix=${HOME}/.local
-
-cp ../config/quicktile.cfg ${HOME}/.config/
+sudo ./setup.py install --prefix=${HOME}/.local
 
 cd ${CW}
