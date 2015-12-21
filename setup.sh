3047f2343fcd0dc9f1594876e90136ecd34b30db
W. Nicholas Greene <wng@csail.mit.edu>
2015-12-21 17:02:09 -0500

    Add pcl ppa.

--- setup.sh
+++ setup.sh
@@ -100,6 +100,9 @@ sudo pip install PyOpenGL PyOpenGL_accelerate
 # Install some research stuff.
 sudo apt-get install -y libeigen3-dev
 sudo apt-get install -y libopencv-dev
+
+sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
+sudo apt-get update
 sudo apt-get install -y libpcl-all-dev
 
 # Install other packages.
