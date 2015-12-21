de07a0426a9c3bde6172e5a5455aede207bfef4a
W. Nicholas Greene <wng@csail.mit.edu>
2015-12-21 17:10:13 -0500

    Remove cd in skype,sh.

--- packages/skype.sh
+++ packages/skype.sh
@@ -2,7 +2,6 @@
 
 # Install skype
 # Taken from: https://askubuntu.com/questions/488053/how-to-install-skype-4-3
-CW=PWD
 
 # Remove some deprecated stuff.
 sudo apt-get remove -y skype skype-bin:i386 skype:i386
@@ -16,4 +15,3 @@ sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) pa
 sudo apt-get update
 sudo apt-get install -y skype
 
-cd ${CW}
