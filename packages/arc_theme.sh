9854696eb583667060a1d7c032bf65a2212bce21
W. Nicholas Greene <wng@csail.mit.edu>
2017-05-29 21:25:53 -0400

    Update arc theme script.

--- packages/arc_theme.sh
+++ packages/arc_theme.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Install arc-theme. After installing, use set using lxappearance.
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+wget -nv http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key -O Release.key
+sudo apt-key add - < Release.key
 sudo apt-get update
 sudo apt-get install -y arc-theme
+
+rm Release.key
