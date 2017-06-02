b9ff077f6888c8d08d1e099208f117d191020eca
W. Nicholas Greene <wng@csail.mit.edu>
2017-06-01 20:03:47 -0400

    Prettify i3blocks.

--- scripts/i3blockscpu.sh
+++ scripts/i3blockscpu.sh
@@ -11,21 +11,18 @@ use warnings;
 use utf8;
 use Getopt::Long;
 
-# default values
+# Color thresholds.
+my $t_norm = 25;
 my $t_warn = 50;
 my $t_crit = 80;
 my $cpu_usage = -1;
 
 sub help {
-    print "Usage: cpu_usage [-w <warning>] [-c <critical>]\n";
-    print "-w <percent>: warning threshold to become yellow\n";
-    print "-c <percent>: critical threshold to become red\n";
+    print "Usage: cpu_usage\n";
     exit 0;
 }
 
-GetOptions("help|h" => \&help,
-           "w=i"    => \$t_warn,
-           "c=i"    => \$t_crit);
+GetOptions("help|h" => \&help);
 
 # Get CPU usage
 $ENV{LC_ALL}="en_US"; # if mpstat is not run under en_US locale, things may break, so make sure it is
@@ -41,14 +38,16 @@ close(MPSTAT);
 $cpu_usage eq -1 and die 'Can\'t find CPU information';
 
 # Print short_text, full_text
-printf "%.2f%%\n", $cpu_usage;
-printf "%.2f%%\n", $cpu_usage;
+printf "%4.1f%%\n", $cpu_usage;
+printf "%4.1f%%\n", $cpu_usage;
 
 # Print color, if needed
 if ($cpu_usage >= $t_crit) {
     print "#FF0000\n";
-    exit 33;
+    # exit 33;
 } elsif ($cpu_usage >= $t_warn) {
+    print "#FF6600\n";
+} elsif ($cpu_usage >= $t_norm) {
     print "#FFFC00\n";
 } else {
     print "#00FF00\n";
