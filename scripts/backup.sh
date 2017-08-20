ad88c811856ff667554db28ed3034a3828faa7a5
W. Nicholas Greene <wnickgreene@gmail.com>
2017-08-19 23:49:36 -0400

    Check if back drive is mounted.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,9 +7,14 @@ OPTIONS=-avhP
 SOURCE_ROOT=/home/wng
 REMOTE_ROOT=${1-/media/wng/wng4tbraid1}
 
-# NOTE THE TRAILING SLASH!
-rsync $OPTIONS $SOURCE_ROOT/Documents/ $REMOTE_ROOT/Documents/
-rsync $OPTIONS $SOURCE_ROOT/Downloads/ $REMOTE_ROOT/Downloads/
-rsync $OPTIONS $SOURCE_ROOT/Music/ $REMOTE_ROOT/Music/
-rsync $OPTIONS $SOURCE_ROOT/Pictures/ $REMOTE_ROOT/Pictures/
-rsync $OPTIONS $SOURCE_ROOT/Videos/ $REMOTE_ROOT/Videos/
+if mount | grep "on $REMOTE_ROOT type" > /dev/null
+then
+  # NOTE THE TRAILING SLASH!
+  rsync $OPTIONS $SOURCE_ROOT/Documents/ $REMOTE_ROOT/Documents/
+  # rsync $OPTIONS $SOURCE_ROOT/Downloads/ $REMOTE_ROOT/Downloads/
+  # rsync $OPTIONS $SOURCE_ROOT/Music/ $REMOTE_ROOT/Music/
+  # rsync $OPTIONS $SOURCE_ROOT/Pictures/ $REMOTE_ROOT/Pictures/
+  # rsync $OPTIONS $SOURCE_ROOT/Videos/ $REMOTE_ROOT/Videos/
+else
+    echo "Remote root $REMOTE_ROOT is not mounted!"
+fi
