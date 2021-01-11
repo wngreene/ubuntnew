efe81b19af452440619a1df596dc6ad5b6dfd1e8
W. Nicholas Greene <wnick.greene@strio.ai>
2021-01-11 18:54:30 -0500

    Comment unneeded stuff for work.

--- setup.sh
+++ setup.sh
@@ -15,17 +15,18 @@ mkdir -p ${HOME}/.local/bin
 mkdir -p ${HOME}/.local/opt
 
 # Copy some scripts to local bin.
-sudo cp scripts/backup.sh ~/.local/bin/
-sudo cp scripts/keepassx_sync.sh ~/.local/bin/
+#sudo cp scripts/backup.sh ~/.local/bin/
+#sudo cp scripts/keepassx_sync.sh ~/.local/bin/
 
 # Copy backup script to cron.daily.
-sudo ln -s ~/.local/bin/backup.sh /etc/cron.daily/backup
+#sudo ln -s ~/.local/bin/backup.sh /etc/cron.daily/backup
 
 # Install terminator.
 sudo apt-get install -y terminator
 
 # Copy config files.
-cp -r .config/* ${HOME}/.config/
+#cp -r .config/* ${HOME}/.config/
+cp -r .config/quicktile.cfg ${HOME}/.config/
 
 # Installing some essential tools.
 echo "Installing some essential tools..."
@@ -35,7 +36,7 @@ sudo apt-get install -y gparted
 sudo apt-get install -y usb-creator-gtk
 sudo apt-get install -y git gitk subversion
 sudo apt-get install -y keepassx
-sudo apt-get install -y rsync grsync #unison unison-gtk
+#sudo apt-get install -y rsync grsync #unison unison-gtk
 sudo apt-get install -y trash-cli
 sudo apt-get install -y git
 sudo apt-get install -y htop
@@ -44,44 +45,44 @@ sudo apt-get install -y silversearcher-ag
 # Other tools.
 echo "Installing misc. tools..."
 sudo apt-get install -y build-essential
-sudo apt-get install -y openjdk-7-jdk 
+#sudo apt-get install -y openjdk-7-jdk 
 sudo apt-get install -y linux-headers-generic
 sudo apt-get install -y gnome-system-monitor
 # sudo apt-get install -y libreoffice
-sudo apt-get install -y vlc xubuntu-restricted-extras libavcodec-extra
-sudo apt-get install -y transmision
+#sudo apt-get install -y vlc xubuntu-restricted-extras libavcodec-extra
+#sudo apt-get install -y transmision
 sudo apt-get install -y cmake cmake-curses-gui
-sudo apt-get install -y libhdf5-dev hdf5-tools hdf5-helpers h5utils
-sudo apt-get install -y liblz4-dev
+#sudo apt-get install -y libhdf5-dev hdf5-tools hdf5-helpers h5utils
+#sudo apt-get install -y liblz4-dev
 sudo apt-get install -y openssh-client openssh-server
-sudo apt-get install -y synergy
+#sudo apt-get install -y synergy
 sudo apt-get install -y geeqie
 
 # Install web stuff.
 echo "Installing web stuff..."
 sudo apt-get install -y chromium-browser
-sudo apt-get install -y pepperflashplugin-nonfree # Flash for chromium.
-sudo apt-get install -y flashplugin-installer # Flash for firefox.
-sudo apt-get install -y icedtea-plugin # Java plugin.
+#sudo apt-get install -y pepperflashplugin-nonfree # Flash for chromium.
+#sudo apt-get install -y flashplugin-installer # Flash for firefox.
+#sudo apt-get install -y icedtea-plugin # Java plugin.
 
 # Latex stuff.
-echo "Installing LaTeX stuff..."
-sudo apt-get install -y texlive
+#echo "Installing LaTeX stuff..."
+#sudo apt-get install -y texlive
 
 # Python stuff.
 echo "Installing Python stuff..."
 sudo apt-get install -y python-pip
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
-sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
-sudo pip install pylint cpplint
+#sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+#sudo pip install pylint cpplint
 
 # Load monitor.
-sudo apt-get install -y indicator-multiload
+#sudo apt-get install -y indicator-multiload
 
 # Install nvidia drivers.
-sudo add-apt-repository -y ppa:xorg-edgers/ppa
-sudo apt-get update
+#sudo add-apt-repository -y ppa:xorg-edgers/ppa
+#sudo apt-get update
 # sudo apt-get install -y nvidia-driver-440
 
 # Install OpenGL.
@@ -89,7 +90,7 @@ sudo apt-get update
 
 # Install other packages.
 ./packages/emacs.sh
-./packages/dropbox.sh
+#./packages/dropbox.sh
 # ./packages/skype.sh
 ./packages/quicktile.sh
 
