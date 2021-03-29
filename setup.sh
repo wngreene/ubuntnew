3a32b52dff8a036a32eec4c33bc8f49e2765855e
W. Nicholas Greene <wnickgreene@gmail.com>
2021-03-29 14:27:47 -0400

    Remove deprecated stuff from setup script.

--- setup.sh
+++ setup.sh
@@ -29,7 +29,6 @@ sudo apt-get install -y gparted
 sudo apt-get install -y usb-creator-gtk
 sudo apt-get install -y git gitk subversion
 sudo apt-get install -y keepassxc
-#sudo apt-get install -y rsync grsync #unison unison-gtk
 sudo apt-get install -y trash-cli
 sudo apt-get install -y git
 sudo apt-get install -y htop
@@ -38,29 +37,12 @@ sudo apt-get install -y silversearcher-ag
 # Other tools.
 echo "Installing misc. tools..."
 sudo apt-get install -y build-essential
-#sudo apt-get install -y openjdk-7-jdk 
 sudo apt-get install -y linux-headers-generic
 sudo apt-get install -y gnome-system-monitor
-# sudo apt-get install -y libreoffice
-#sudo apt-get install -y vlc xubuntu-restricted-extras libavcodec-extra
-#sudo apt-get install -y transmision
 sudo apt-get install -y cmake cmake-curses-gui
-#sudo apt-get install -y libhdf5-dev hdf5-tools hdf5-helpers h5utils
-#sudo apt-get install -y liblz4-dev
 sudo apt-get install -y openssh-client openssh-server
 #sudo apt-get install -y synergy
 sudo apt-get install -y geeqie
-
-# Install web stuff.
-# echo "Installing web stuff..."
-# sudo apt-get install -y chromium-browser
-#sudo apt-get install -y pepperflashplugin-nonfree # Flash for chromium.
-#sudo apt-get install -y flashplugin-installer # Flash for firefox.
-#sudo apt-get install -y icedtea-plugin # Java plugin.
-
-# Latex stuff.
-#echo "Installing LaTeX stuff..."
-#sudo apt-get install -y texlive
 
 # Load monitor.
 sudo apt-get install -y indicator-multiload
